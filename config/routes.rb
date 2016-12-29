0bea8255a7ce0c8edb87fc9a725a3cf57aabc1e8
emikod@mac.com <emikod@mac.com>
2016-12-28 22:05:12 -0700

    Adds bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'menu/index'
 
-  get 'menu/about'
+  get 'about' => 'menu#about'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
