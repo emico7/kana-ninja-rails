b3a5860d0f99fc56dfbd13a6e0112823ae6ac8b2
emikod@mac.com <emikod@mac.com>
2017-01-08 22:15:51 -0700

    Disables turbolinks, removes some commented lines

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,14 +1,3 @@
-// Level 1 – specific set of words/images, go through in order
-// Collect words/images, make/find recordings
-// Level 2 - randomly choose from level 2 set of words
-// Collect words/images, make/find recordings
-// Level 3 - randomly choose from level 3 set of words
-// Level 4 - randomly choose from level 4 set of words
-// Level 5 - randomly choose from level 5 set of words
-
-// $(document).ready(function() {
-// });
-
 // Game object
 // score
 
@@ -53,21 +42,6 @@ function Question(answer, letters) {
     this.answer = answer;
     this.letters = letters;
 }
-
-// // var currentExercise = new Exercise("house.jpg",  )
-//
-// // letter object
-// // letter
-// // audio file
-//
-// function Letter(letter, audioFile) {
-//     this.letter = letter;
-//     this.audioFile = new buzz.sound("assets/audio/" + audioFile, {
-//         formats: [ 'm4a' ],
-//         preload: true
-//     });
-// }
-//
 
 $(document).ready(function() {
 // $(document).on('ready page:load', function() {
